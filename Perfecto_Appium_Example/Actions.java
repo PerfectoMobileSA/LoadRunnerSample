cda38529d4bb2ec51d213689e203ae267bcbc8c9
genesisthomas <genesist@perfectomobile.com>
2019-11-19 13:57:40 +0530

    init

---
+++ Perfecto_Appium_Example/Actions.java
@@ -0,0 +1,171 @@
+/*
+ * LoadRunner Java script. (Build: _build_number_)
+ * 
+ * Script Description: 
+ *                     
+ */
+
+import lrapi.lr;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.html5.*;
+import org.openqa.selenium.logging.*;
+import org.openqa.selenium.remote.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.Cookie.Builder;
+
+import io.appium.java_client.*;
+import io.appium.java_client.android.*;
+import io.appium.java_client.ios.*;
+
+public class Actions
+{
+
+	public int init() throws Throwable {
+		return 0;
+	}//end of init
+
+
+	public int action() throws Throwable {
+		System.out.println("Run started");
+        
+        String browserName = "mobileOS";
+        DesiredCapabilities capabilities = new DesiredCapabilities(browserName, "", Platform.ANY);
+        String host = "demo.perfectomobile.com";
+        capabilities.setCapability("securityToken","<SecurityToken>");
+		
+        
+        //TODO: Change your device ID
+        capabilities.setCapability("deviceName", "<DeviceUDID>");
+        
+        // Use the automationName capability to define the required framework - Appium (this is the default) or PerfectoMobile.
+        capabilities.setCapability("automationName", "Appium");
+
+        
+        //AndroidDriver driver = new AndroidDriver(new URL("https://" + host + "/nexperience/perfectomobile/wd/hub"), capabilities);
+        IOSDriver driver = new IOSDriver(new URL("https://" + host + "/nexperience/perfectomobile/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        
+        try {
+        	lr.start_transaction("Navigate to Google"); //Creates a transaction in loadrunner
+        	navigateToPage(driver,"https://www.google.com");
+        	lr.end_transaction("Navigate to Google", lr.AUTO); //Match this with the start_transaction
+        	
+        	lr.start_transaction("Search Perfecto");
+    		setText(driver, By.name("q"), "perfecto");
+    		lr.end_transaction("Search Perfecto", lr.AUTO);
+    		
+    		lr.start_transaction("Search Codeless");
+    		setText(driver, By.name("q"), "sauce");
+    		lr.end_transaction("Search Codeless", lr.AUTO);
+    		
+    		lr.start_transaction("Search Selenium");
+    		setText(driver, By.name("q"), "Selenium");
+    		lr.end_transaction("Search Selenium", lr.AUTO);
+    		
+    		lr.start_transaction("Search Appium");
+    		setText(driver, By.name("q"), "Appium");
+    		lr.end_transaction("Search Appium", lr.AUTO);
+    		
+    		lr.start_transaction("Search Automation");
+    		setText(driver, By.name("q"), "Automation");
+    		lr.end_transaction("Search Automation", lr.AUTO);
+        	
+    		//uxTimer(driver, "automation"); //uxTimer can also be used if you are leveraging Perfecto's uxTimer
+
+        } catch (Exception e) {
+            
+        } finally {
+            try {
+                driver.quit();
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+        System.out.println("Run ended");
+        return 0;
+	}//end of action
+
+
+	public int end() throws Throwable {
+		return 0;
+	}//end of end
+	
+    
+	public void click(RemoteWebDriver driver, By by) {
+		WebElement element = new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(by));
+		element.click();
+	}
+	public String getTextOCR(RemoteWebDriver driver, String needle) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("content", needle);
+		params.put("timeout", "20");
+		String resultString = (String) driver.executeScript("mobile:checkpoint:text", params);
+		return resultString;
+	}
+	public static void setText(RemoteWebDriver driver, By by, String text) {
+		WebElement element = new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(by));
+		element.click();
+		element.clear();
+		element.sendKeys(text);
+	}
+	public static void navigateToPage(RemoteWebDriver driver, String url) {
+		driver.get(url);
+	}
+	
+	public String startApp(RemoteWebDriver driver, String identifier) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("identifier", identifier);
+		String resultString = (String) driver.executeScript("mobile:application:open", params);
+		return resultString;
+	}
+	
+	public String closeApp(RemoteWebDriver driver, String identifier) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("identifier", identifier);
+		String resultString = (String) driver.executeScript("mobile:application:close", params);
+		return resultString;
+	}
+
+	public static void clickText(RemoteWebDriver driver, String text) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("content", text);
+		params.put("timeout", "20");
+		String resultString = (String) driver.executeScript("mobile:text:select", params);
+
+	}
+	
+	public static void uxTimer(RemoteWebDriver driver, String timerName) {
+		Map<String, Object> params = new HashMap<>();
+		params = new HashMap<>();
+		params.put("timerId", timerName);
+		params.put("timerType", "ux");
+		long timeEx = (long) driver.executeScript("mobile:timer:info", params);
+		lr.set_transaction(timerName, timeEx, lr.PASS);
+	}
+	
+		public static void uxTimer(RemoteWebDriver driver, String timerName, long thresold) {
+		Map<String, Object> params = new HashMap<>();
+		params = new HashMap<>();
+		params.put("timerId", timerName);
+		params.put("timerType", "ux");
+		long timeEx = (long) driver.executeScript("mobile:timer:info", params);
+		if (timeEx > thresold) {
+			lr.set_transaction(timerName, timeEx, lr.FAIL);
+		} else {
+			lr.set_transaction(timerName, timeEx, lr.PASS);
+		}
+	}
+	
+	
+	
+	
+
+}
